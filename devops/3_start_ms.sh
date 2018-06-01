1ff7d136e8ed2e34c99da0405595940d0f5f2f01
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-01 17:17:20 +0700

    add script to auto start microservice

---
+++ devops/3_start_ms.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_PATH=`pwd`
+
+if [ $# -gt 0 ] && [ $1 -eq 1 ]; then
+  update-lib
+  for MS in `cat list-ms.txt`; do
+    echo "==== build & compile for ${MS}"
+    cd ${CURRENT_PATH}/../../$MS
+    git pull && mvn clean install
+  done
+fi
+
+for MS in `cat list-ms.txt`; do
+  if [ -e ${CURRENT_PATH}/run.pid ] && [ `grep -c $MS ${CURRENT_PATH}/run.pid 2>/dev/null` -ne 0 ]; then
+      PID=`grep $MS ${CURRENT_PATH}/run.pid | cut -d' ' -f2`
+      echo "==== ${MS} running with pid $PID"
+  elif [ ! -e ${CURRENT_PATH}/run.pid ] || [ `grep -c $MS ${CURRENT_PATH}/run.pid 2>/dev/null` -eq 0 ]; then
+      cd ${CURRENT_PATH}/../../$MS
+      nohup java -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/run.pid
+      echo "==== start java in background process for ${MS} $!"
+  fi
+done
