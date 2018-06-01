5fc24649b2800bd8a69e84b4aadb312c90c98bc0
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-01 19:13:12 +0700

    updated structure folder

--- devops/1_bootstrap.sh
+++ devops/1_bootstrap.sh
@@ -5,6 +5,7 @@ couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bu
 couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bucket temporary --bucket-type couchbase --bucket-ramsize 100 --enable-flush 1
 couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bucket hotels --bucket-type couchbase --bucket-ramsize 100 --enable-flush 1
 couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bucket locations --bucket-type couchbase --bucket-ramsize 100 --enable-flush 1
+couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bucket markets --bucket-type couchbase --bucket-ramsize 100 --enable-flush 1
 couchbase-cli bucket-create -c couchbase:8091 -u Administrator -p 090CE11ce --bucket ms_translations --bucket-type couchbase --bucket-ramsize 100 --enable-flush 1
 sleep 15
-cbq -f ./init-index.n1ql -engine http://couchbase:8091 -u Administrator -p 090CE11ce
+cbq -f ./src/init-index.n1ql -engine http://couchbase:8091 -u Administrator -p 090CE11ce
--- devops/3_start_ms.sh
+++ devops/3_start_ms.sh
@@ -4,20 +4,20 @@ CURRENT_PATH=`pwd`
 
 if [ $# -gt 0 ] && [ $1 -eq 1 ]; then
   update-lib
-  for MS in `cat list-ms.txt`; do
+  for MS in `cat ./tmp/list-ms.txt`; do
     echo "==== build & compile for ${MS}"
     cd ${CURRENT_PATH}/../../$MS
     git pull && mvn clean install
   done
 fi
 
-for MS in `cat list-ms.txt`; do
-  if [ -e ${CURRENT_PATH}/run.pid ] && [ `grep -c $MS ${CURRENT_PATH}/run.pid 2>/dev/null` -ne 0 ]; then
-      PID=`grep $MS ${CURRENT_PATH}/run.pid | cut -d' ' -f2`
+for MS in `cat ./tmp/list-ms.txt`; do
+  if [ -e ${CURRENT_PATH}/tmp/run.pid ] && [ `grep -c $MS ${CURRENT_PATH}/tmp/run.pid 2>/dev/null` -ne 0 ]; then
+      PID=`grep $MS ${CURRENT_PATH}/tmp/run.pid | cut -d' ' -f2`
       echo "==== ${MS} running with pid $PID"
-  elif [ ! -e ${CURRENT_PATH}/run.pid ] || [ `grep -c $MS ${CURRENT_PATH}/run.pid 2>/dev/null` -eq 0 ]; then
+  elif [ ! -e ${CURRENT_PATH}/tmp/run.pid ] || [ `grep -c $MS ${CURRENT_PATH}/tmp/run.pid 2>/dev/null` -eq 0 ]; then
       cd ${CURRENT_PATH}/../../$MS
-      nohup java -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/run.pid
+      nohup java -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/tmp/run.pid
       echo "==== start java in background process for ${MS} $!"
   fi
 done
