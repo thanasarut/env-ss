58410202af7e3b6d411d075fa51e216b1e75d634
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-04 11:24:43 +0700

    fixe 2_init_data.sh

--- devops/2_init_data.sh
+++ devops/2_init_data.sh
@@ -2,11 +2,11 @@
 
 cbq --script="select * from authenticate where meta().id = 'email::admin@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ./tmp/temp.json
 
-echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'$(cat temp.json)');' > ./tmp/init-data.n1ql
+echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'$(cat ./tmp/temp.json)');' > ./tmp/init-data.n1ql
 
 cbq --script="select * from authenticate where meta().id = 'permission::vick.thanasarut@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ./tmp/temp.json
 
-echo 'insert into authenticate (key,value) values("permission::admin@sunseries.travel",'$(cat temp.json)');' >> ./tmp/init-data.n1ql
+echo 'insert into authenticate (key,value) values("permission::admin@sunseries.travel",'$(cat ./tmp/temp.json)');' >> ./tmp/init-data.n1ql
 
 rm ./tmp/temp.json
 
