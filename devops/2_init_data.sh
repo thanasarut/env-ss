1e72778f23e7a9981fc8a988c61f812b38ecab01
Vick Thanasarut <vick.thanasarut@TheiMac2.local>
2018-06-05 11:00:54 +0700

    modify 2_init_data

--- devops/2_init_data.sh
+++ devops/2_init_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir tmp
 cbq --script="select * from authenticate where meta().id = 'email::admin@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ./tmp/temp.json
 
 echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'$(cat ./tmp/temp.json)');' > ./tmp/init-data.n1ql
@@ -13,3 +14,4 @@ rm ./tmp/temp.json
 cbq -f ./tmp/init-data.n1ql -engine http://couchbase:8091 -u Administrator -p 090CE11ce
 
 rm ./tmp/init-data.n1ql
+rm -fr ./tmp
