10ab22bbf348782e4c2b4bc8a6d40050225800f7
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-06 10:28:49 +0700

    update 3_start_ms.sh

--- devops/3_start_ms.sh
+++ devops/3_start_ms.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 CURRENT_PATH=`pwd`
-SRC_PATH=./src
+MS_SRC_CODE_PATH=$HOME/sunseries
+SRC=src
+TEMP=tmp
 
+if [ ! -e ./${TEMP} ]; then
+  mkdir ./${TEMP}
+fi
 if [ $# -gt 0 ] && [ $1 -eq 1 ]; then
   update-lib
-  for MS in `cat ${SRC_PATH}/list-ms.txt`; do
+  for MS in `cat ./${SRC}/list-ms.txt`; do
     echo "==== build & compile for ${MS}"
-    cd ${CURRENT_PATH}/../../$MS
+    cd ${MS_SRC_CODE_PATH}/$MS
     git pull && mvn clean install
   done
 fi
 
-for MS in `cat ${SRC_PATH}/list-ms.txt`; do
-  if [ -e ${CURRENT_PATH}/tmp/run.pid ] && [ `grep -c $MS ${CURRENT_PATH}/tmp/run.pid 2>/dev/null` -ne 0 ]; then
-      PID=`grep $MS ${CURRENT_PATH}/tmp/run.pid | cut -d' ' -f2`
+for MS in `cat ${SRC}/list-ms.txt`; do
+  if [ -e ${CURRENT_PATH}/${TEMP}/run.pid ] && [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -ne 0 ]; then
+      PID=`cat ${CURRENT_PATH}/${TEMP}/run.pid | awk '{print $0, $1}' | cut -d' ' -f2,3 | grep $MS$ | cut -d' ' -f1`
       echo "==== ${MS} running with pid $PID"
-  elif [ ! -e ${CURRENT_PATH}/tmp/run.pid ] || [ `grep -c $MS ${CURRENT_PATH}/tmp/run.pid 2>/dev/null` -eq 0 ]; then
-      cd ${CURRENT_PATH}/../../$MS
-      nohup java -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/tmp/run.pid
+  elif [ ! -e ${CURRENT_PATH}/${TEMP}/run.pid ] || [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -eq 0 ]; then
+      cd ${MS_SRC_CODE_PATH}/$MS
+      nohup java -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/${TEMP}/run.pid
       echo "==== start java in background process for ${MS} $!"
   fi
 done
