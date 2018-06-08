458986541ad3ca5960980e8675d734764ece8977
Thanasarut Sasismit <thanasarut@Thanasita-MBP.local>
2018-06-08 18:43:39 +0700

    update

--- devops/2_init_data.sh
+++ devops/2_init_data.sh
@@ -3,13 +3,21 @@
 TEMP_PATH=./tmp
 
 mkdir ${TEMP_PATH}
+echo -n "get user from int"
 cbq --script="select * from authenticate where meta().id = 'email::admin@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ${TEMP_PATH}/temp.json
+echo "...done"
 
+echo -n "insert user to local"
 echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'`cat ${TEMP_PATH}/temp.json`');' > ${TEMP_PATH}/init-data.n1ql
+echo "...done"
 
+echo -n "get permission from int"
 cbq --script="select * from authenticate where meta().id = 'permission::vick.thanasarut@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ${TEMP_PATH}/temp.json
+echo "...done"
 
+echo -n "insert permission to local"
 echo 'insert into authenticate (key,value) values("permission::admin@sunseries.travel",'`cat ${TEMP_PATH}/temp.json`');' >> ${TEMP_PATH}/init-data.n1ql
+echo "...done"
 
 rm ${TEMP_PATH}/temp.json
 
