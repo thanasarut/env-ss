b4dba27bbef5133d1c35ef5b0fcd374e27d2d998
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-08 15:26:19 +0700

    fixed

--- devops/2_init_data.sh
+++ devops/2_init_data.sh
@@ -5,7 +5,7 @@ TEMP_PATH=./tmp
 mkdir ${TEMP_PATH}
 cbq --script="select * from authenticate where meta().id = 'email::admin@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ${TEMP_PATH}/temp.json
 
-echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'`cat ${TEMP_PATH}/temp.json)`';' > ${TEMP_PATH}/init-data.n1ql
+echo 'insert into authenticate (key,value) values("email::admin@sunseries.travel",'`cat ${TEMP_PATH}/temp.json`');' > ${TEMP_PATH}/init-data.n1ql
 
 cbq --script="select * from authenticate where meta().id = 'permission::vick.thanasarut@sunseries.travel'" -engine http://couchbase.int.bkk1.sunseries.travel:8091 -u Administrator -p 090CE11ce | awk 'NR>3 {print}' | jq -c '.results[0].authenticate' > ${TEMP_PATH}/temp.json
 
