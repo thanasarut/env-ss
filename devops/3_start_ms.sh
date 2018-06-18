edf588932c846a9a6339f946130d6a6e247941ff
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-06-18 09:40:44 +0700

    update

--- devops/3_start_ms.sh
+++ devops/3_start_ms.sh
@@ -10,14 +10,14 @@ if [ ! -e ./${TEMP} ]; then
 fi
 if [ $# -gt 0 ] && [ $1 -eq 1 ]; then
   update-lib
-  for MS in `cat ./${SRC}/list-ms.txt`; do
+  for MS in `cat ./${SRC}/list-ms.txt | grep -v "^#"`; do
     echo "==== build & compile for ${MS}"
     cd ${MS_SRC_CODE_PATH}/$MS
     git pull && mvn clean install
   done
 fi
 
-for MS in `cat ${SRC}/list-ms.txt`; do
+for MS in `cat ${SRC}/list-ms.txt | grep -v "^#"`; do
   if [ -e ${CURRENT_PATH}/${TEMP}/run.pid ] && [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -ne 0 ]; then
       PID=`cat ${CURRENT_PATH}/${TEMP}/run.pid | awk '{print $0, $1}' | cut -d' ' -f2,3 | grep $MS$ | cut -d' ' -f1`
       echo "==== ${MS} running with pid $PID"
