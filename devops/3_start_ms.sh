6da77f0ca1a45a234dc0c7f3b8063ee4e2b916a2
Thanasarut Sasismit <vick.thanasarut@sol.tech>
2018-07-17 11:33:31 +0700

    update script 3_start_ms.sh

--- devops/3_start_ms.sh
+++ devops/3_start_ms.sh
@@ -19,11 +19,13 @@ fi
 
 for MS in `cat ${SRC}/list-ms.txt | grep -v "^#"`; do
   if [ `ps -ef | grep jar$ | grep -c $MS` -eq 0 ]; then
+  # no process of that ms running
     cd ${MS_SRC_CODE_PATH}/$MS
     nohup java -Xmx256m -jar target/*.jar > /dev/null 2>&1 &
+    echo "==== start java in background process for ${MS} $!"
     if [ -e ${CURRENT_PATH}/${TEMP}/run.pid ]; then
+    # log file of running is exist
       OLD_PID=`cat ${CURRENT_PATH}/${TEMP}/run.pid | awk '/$MS / {print $2}'`
-	echo ${OLD_PID}
       if [ "${OLD_PID}" != "" ]; then
         sed -i '' 's/ ${OLD_PID}$/ $!/g' ${CURRENT_PATH}/${TEMP}/run.pid
       else
@@ -32,14 +34,22 @@ for MS in `cat ${SRC}/list-ms.txt | grep -v "^#"`; do
     else
         echo "$MS $!" >> ${CURRENT_PATH}/${TEMP}/run.pid
     fi
-    echo "==== start java in background process for ${MS} $!"
-  elif [ -e ${CURRENT_PATH}/${TEMP}/run.pid ] && [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -ne 0 ]; then
-    PID=`cat ${CURRENT_PATH}/${TEMP}/run.pid | awk '{print $0, $1}' | cut -d' ' -f2,3 | grep $MS$ | cut -d' ' -f1`
+  elif [ -e ${CURRENT_PATH}/${TEMP}/run.pid ]; then
+  # process still running && log file of running is exist
+    if [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -ne 0 ]; then
+    # found PID of process in log file
+      PID=`cat ${CURRENT_PATH}/${TEMP}/run.pid | awk '{print $0, $1}' | cut -d' ' -f2,3 | grep $MS$ | cut -d' ' -f1`
+      echo "==== ${MS} running with pid $PID"
+    else
+    # not found PID of proces in log file
+      PID=`ps -ef | grep jar$ | grep $MS | awk '{print $2}'`
+      echo "$MS $PID" >> ${CURRENT_PATH}/${TEMP}/run.pid
+      echo "==== ${MS} running with pid $PID"
+    fi
+  else
+  # process still running && no log file exist
+    PID=`ps -ef | grep jar$ | grep $MS | awk '{print $2}'`
+    echo "$MS $PID" >> ${CURRENT_PATH}/${TEMP}/run.pid
     echo "==== ${MS} running with pid $PID"
-  elif [ ! -e ${CURRENT_PATH}/${TEMP}/run.pid ] || [ `cat ${CURRENT_PATH}/${TEMP}/run.pid | cut -d' ' -f1 | grep -c $MS$ 2>/dev/null` -eq 0 ]; then
-    cd ${MS_SRC_CODE_PATH}/$MS
-echo "no file run.pid exists"
-    nohup java -Xmx256m -jar target/*.jar > /dev/null 2>&1 & echo "$MS $!" >> ${CURRENT_PATH}/${TEMP}/run.pid
-    echo "==== start java in background process for ${MS} $!"
   fi
 done
